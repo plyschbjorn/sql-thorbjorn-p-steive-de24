8648dab5d43ee69f1731b1aa9896b2042be320b4
Plyschbjörn <plysch@proton.me>
2024-12-03 10:27:34 +0100

    change load sakila

--- 10_set_operations/sql/1_load_sakila.sql
+++ 10_set_operations/sql/1_load_sakila.sql
@@ -1,4 +1,6 @@
 INSTALL sqlite;
 LOAD sqlite;
 
-CALL sqlite_attach('/Users/plysch/Library/Mobile Documents/com~apple~CloudDocs/STI/3. SQL/github/sql-thorbjorn-p-steive-de24/10_set_operations/data/sqlite-sakila.db');
+CALL sqlite_attach('/Users/plysch/Documents/github/sql-thorbjorn-p-steive-de24/10_set_operations/data/sqlite-sakila.db');
+
+-- CALL sqlite_attach('/Users/plysch/Library/Mobile Documents/com~apple~CloudDocs/STI/3. SQL/github/sql-thorbjorn-p-steive-de24/10_set_operations/data/sqlite-sakila.db');
